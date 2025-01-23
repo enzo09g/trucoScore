e1befe119318a7213f087438e813717fe1e3f0dc
Enzo Gómez <enzoeegv@gmail.com>
2025-01-23 03:15:52 -0300

    Deploy logrado!

--- src/Components/Tanteador.jsx
+++ src/Components/Tanteador.jsx
@@ -17,8 +17,8 @@ export function Tanteador({ InitialTantos }) {
     const cantidadRepeticiones = Tantos > 5 ? Math.floor((Tantos - 1) / 5) : 0;
     const srcNumero = calcularPuntaje(Tantos)
 
-    const fotoSrc = `/public/Img/fosforos/${srcNumero} palito.png`
-    const fotoSrc5 = '/public/Img/fosforos/${srcNumero} palito.png'
+    const fotoSrc = `/Img/${srcNumero} palito.png`
+    const fotoSrc5 = '/Img/5 palito.png'
 
     return (
         <div className="w-full mt-8 min-h-24 bg-gray-100 opacity-80 rounded-3xl shadow-2xl backdrop-blur-xl flex overflow-x-hidden flex-wrap">
