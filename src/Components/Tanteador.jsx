55528377cdc66ec0d44113f96e588ec8e6162082
Enzo Gómez <enzoeegv@gmail.com>
2025-01-23 02:30:07 -0300

    Deploy numero ni me acuerdo

--- src/Components/Tanteador.jsx
+++ src/Components/Tanteador.jsx
@@ -18,8 +18,8 @@ export function Tanteador({ InitialTantos }) {
     const cantidadRepeticiones = Tantos > 5 ? Math.floor((Tantos - 1) / 5) : 0;
     const srcNumero = calcularPuntaje(Tantos)
 
-    const fotoSrc = `src/Img/fosforos/${srcNumero} palito.png`
-    const fotoSrc5 = `src/Img/fosforos/5 palito.png`
+    const fotoSrc = `/src/Img/fosforos/${srcNumero} palito.png`
+    const fotoSrc5 = `/src/Img/fosforos/5 palito.png`
 
     return (
         <div className="w-full mt-8 min-h-24 bg-gray-100 opacity-80 rounded-3xl shadow-2xl backdrop-blur-xl flex overflow-x-hidden flex-wrap">
